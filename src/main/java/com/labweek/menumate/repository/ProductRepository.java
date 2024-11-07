65dae9cab4b62a4f7ac7c8f96780f5e21954654e
Jagadeeshwaran07 <waran.i.am@gmail.com>
2024-11-08 00:07:08 +0530

    Implemented Delete and Get category

--- src/main/java/com/labweek/menumate/repository/ProductRepository.java
+++ src/main/java/com/labweek/menumate/repository/ProductRepository.java
@@ -4,7 +4,11 @@ import com.labweek.menumate.entity.NewProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<NewProductEntity, Long> {
     // Custom query methods can be added if needed
+    List<NewProductEntity> findByCategory(String category);
+
 }
